0f680b8fd9b4fa7633acd763a6a5614184b1628d
Wakotu <442444286@qq.com>
2024-04-15 14:55:41 +0800

    remove extra imports

--- src/autotrain/cli/autotrain.py
+++ src/autotrain/cli/autotrain.py
@@ -1,17 +1,19 @@
 import argparse
 
 from .. import __version__
-from .run_api import RunAutoTrainAPICommand
-from .run_app import RunAutoTrainAppCommand
-from .run_dreambooth import RunAutoTrainDreamboothCommand
-from .run_image_classification import RunAutoTrainImageClassificationCommand
+
+# from .run_api import RunAutoTrainAPICommand
+# from .run_app import RunAutoTrainAppCommand
+# from .run_dreambooth import RunAutoTrainDreamboothCommand
+# from .run_image_classification import RunAutoTrainImageClassificationCommand
 from .run_llm import RunAutoTrainLLMCommand
-from .run_seq2seq import RunAutoTrainSeq2SeqCommand
-from .run_setup import RunSetupCommand
-from .run_spacerunner import RunAutoTrainSpaceRunnerCommand
-from .run_tabular import RunAutoTrainTabularCommand
-from .run_text_classification import RunAutoTrainTextClassificationCommand
-from .run_token_classification import RunAutoTrainTokenClassificationCommand
+
+# from .run_seq2seq import RunAutoTrainSeq2SeqCommand
+# from .run_setup import RunSetupCommand
+# from .run_spacerunner import RunAutoTrainSpaceRunnerCommand
+# from .run_tabular import RunAutoTrainTabularCommand
+# from .run_text_classification import RunAutoTrainTextClassificationCommand
+# from .run_token_classification import RunAutoTrainTokenClassificationCommand
 
 
 def main():
@@ -27,17 +29,17 @@ def main():
     commands_parser = parser.add_subparsers(help="commands")
 
     # Register commands
-    RunAutoTrainAppCommand.register_subcommand(commands_parser)
     RunAutoTrainLLMCommand.register_subcommand(commands_parser)
-    RunSetupCommand.register_subcommand(commands_parser)
-    RunAutoTrainDreamboothCommand.register_subcommand(commands_parser)
-    RunAutoTrainAPICommand.register_subcommand(commands_parser)
-    RunAutoTrainTextClassificationCommand.register_subcommand(commands_parser)
-    RunAutoTrainImageClassificationCommand.register_subcommand(commands_parser)
-    RunAutoTrainTabularCommand.register_subcommand(commands_parser)
-    RunAutoTrainSpaceRunnerCommand.register_subcommand(commands_parser)
-    RunAutoTrainSeq2SeqCommand.register_subcommand(commands_parser)
-    RunAutoTrainTokenClassificationCommand.register_subcommand(commands_parser)
+    # RunAutoTrainAppCommand.register_subcommand(commands_parser)
+    # RunSetupCommand.register_subcommand(commands_parser)
+    # RunAutoTrainDreamboothCommand.register_subcommand(commands_parser)
+    # RunAutoTrainAPICommand.register_subcommand(commands_parser)
+    # RunAutoTrainTextClassificationCommand.register_subcommand(commands_parser)
+    # RunAutoTrainImageClassificationCommand.register_subcommand(commands_parser)
+    # RunAutoTrainTabularCommand.register_subcommand(commands_parser)
+    # RunAutoTrainSpaceRunnerCommand.register_subcommand(commands_parser)
+    # RunAutoTrainSeq2SeqCommand.register_subcommand(commands_parser)
+    # RunAutoTrainTokenClassificationCommand.register_subcommand(commands_parser)
 
     args = parser.parse_args()
 
